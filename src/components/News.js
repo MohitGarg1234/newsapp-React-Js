6561bf1d039213efc09c4bd89980c8c92b387b8b
Mohit Garg <mgarg1557@gmail.com>
2023-08-27 17:56:19 +0530

    changes

--- src/components/News.js
+++ src/components/News.js
@@ -62,7 +62,7 @@ const News = (props)=>{
  
         return (
             <>
-                <h1 className="text-center" style={{ margin: '55px 0px' }}>NewsMonkey - Top {(props.category)} Headlines</h1>
+                <h1 className="text-center" style={{ margin: '55px 0px' }}>NewsEveryday - Top {(props.category)} Headlines</h1>
                 {/* {loading && <Spinner />} */}
                 <InfiniteScroll
                     dataLength={articles.length}
